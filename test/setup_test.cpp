e0bf0f902250b1a9e2f5bd2d1bc75d433a942179
Rainier Barrett <rbarret8@ur.rochester.edu>
2016-12-06 20:51:00 -0500

    need to put in a setup test for input and stuff. got to address how to take in a potential file again...

---
+++ test/setup_test.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "langevin2D.hpp"
+#include "catch.hpp"
+#include <iomanip>
+
+using namespace langevin2D;
+
+SCENARIO( "Can properly set up the Langevin class from a 2D input file.", "[setup]"){
+  
+
+}

240e2ecd7ac49dfff4940b25c06995ad5ad40742
Rainier Barrett <rbarret8@ur.rochester.edu>
2016-12-09 14:59:14 -0500

    Finished test for parameter reader function.

--- test/read_test.cpp
+++ test/read_test.cpp
@@ -18,6 +18,10 @@ SCENARIO( "Can properly parse our input file.", "[input]"){
       THEN( "The class variables should be set."){
 	REQUIRE(abs(integrator.T - 10.0) < EPSILON);
 	REQUIRE(abs(integrator.lambda - 0.55) < EPSILON);
+	REQUIRE(abs(integrator.dt - 0.05) < EPSILON);
+	REQUIRE(abs(integrator.tot_time - 4.0) < EPSILON);
+	REQUIRE(integrator.potential_file_name == "test_pot.txt");
+	REQUIRE(integrator.particle_file_name == "test_particles_1.txt");
       }
     }
   }

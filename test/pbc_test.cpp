72b426f827ab846f3228a895853499269e4fc2e3
Rainier Barrett <rbarret8@ur.rochester.edu>
2016-12-11 19:29:37 -0500

    Updated the PBC tester, now with full branch coverage.

--- test/pbc_test.cpp
+++ test/pbc_test.cpp
@@ -13,12 +13,20 @@ SCENARIO( "Can properly handle PBC x and y setting.", "[PBCs]"){
     
     WHEN("We set the particle's position outside the bounds."){
       integrator.read_params(testfile);
+      integrator.particles[0]->set_x(1.0, -5.0, 5.0);
       integrator.particles[0]->set_x(7.0, -5.0, 5.0);//these are out of bounds
+      integrator.particles[0]->set_y(1.0 -5.0, 5.0);//this is to get coverage on the 'else' in the setter
       integrator.particles[0]->set_y(-6.0, -5.0, 5.0);
 
       THEN( "The particle position should 'loop'."){
 	REQUIRE(abs(integrator.particles[0]->x + 3.0) < EPSILON);
 	REQUIRE(abs(integrator.particles[0]->y - 4.0) < EPSILON);
+      }
+      AND_THEN("The positions should loop the other way, too."){
+	integrator.particles[0]->set_x(-8.0, -5.0, 5.0);
+	integrator.particles[0]->set_y(9.0, -5.0, 5.0);
+	REQUIRE(abs(integrator.particles[0]->x - 2.0) < EPSILON);
+	REQUIRE(abs(integrator.particles[0]->y + 1.0) < EPSILON);
       }
     }
   }
